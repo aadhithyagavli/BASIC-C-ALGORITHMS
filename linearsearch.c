8d9d1f118b1c44241069d7430f742e843b881211
Aadhithya Gavli <aadhithyagavli@gmail.com>
2026-01-28 18:24:27 +0530

    Fix formatting and clean up linear search code
    Removed the variables which were not in use

--- linearsearch.c
+++ linearsearch.c
@@ -5,7 +5,6 @@ int main()
     int arr[] = {1,2,3,4,5,6,7,8,9,10};
     int n = sizeof(arr) / sizeof(arr[0]);
     int found = -1;
-    int low =0 , high = n-1;
     int key;
 
 
@@ -28,6 +27,7 @@ int main()
     {
         printf("%d is not in the array\n",key);
     }
+
 
 
 
